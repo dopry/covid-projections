4196e56192288680ecb86b378b9dfbbf15ba4e34
Fiskell <stephen.fiskell@gmail.com>
2020-03-26 18:58:13 -0700

    Add new Call To Action text table

---
+++ src/assets/images/capacityIcons.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const WarnLimitedAction = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+      fill="#FC374D"
+    />
+  </svg>
+);
+
+const WarnSocialDistancing = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM11 15H9V13H11V15ZM11 11H9V5H11V11Z"
+      fill="#FD9026"
+    />
+  </svg>
+);
+
+const CheckShelterInPlace = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
+      fill="#31E8BC"
+    />
+  </svg>
+);
+
+export { WarnLimitedAction, WarnSocialDistancing, CheckShelterInPlace };
--- src/screens/ModelPage/CallToAction/CallToAction.js
+++ src/screens/ModelPage/CallToAction/CallToAction.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { INTERVENTIONS } from 'enums';
+import { INTERVENTIONS, INTERVENTION_COLOR_MAP } from 'enums';
+import Typography from '@material-ui/core/Typography';
 import Callout from 'components/Callout/Callout';
+import {
+  WarnLimitedAction,
+  WarnSocialDistancing,
+  CheckShelterInPlace,
+} from 'assets/images/capacityIcons';
 
 const LAST_DATES_CALLOUT_COLORS = {
   // Array is [fill color, border color]
@@ -22,12 +28,14 @@ const CallToAction = ({ interventions, currentIntervention }) => {
 
   const model = interventionToModel[currentIntervention];
 
-  let actionText, actionDateRange;
+  let actionText, shortActionText, capacityIcon, actionDateRange;
   if (
     !model.dateOverwhelmed ||
     model.dateOverwhelmed - new Date() > ONE_HUNDRED_DAYS
   ) {
     actionText = `${currentIntervention} projected to successfully delay hospital overload by greater than 3 months.`;
+    shortActionText = `No overload projected`;
+    capacityIcon = <CheckShelterInPlace />;
   } else {
     actionText = `To prevent hospital overload, ${suggestedIntervention()} must be implemented by:`;
     const earlyDate = new Date(model.dateOverwhelmed.getTime() - 14 * DAYS);
@@ -37,20 +45,113 @@ const CallToAction = ({ interventions, currentIntervention }) => {
         {formatDate(earlyDate)} to {formatDate(lateDate)}
       </div>
     );
+    shortActionText = `Overload projected between ${formatDate(
+      earlyDate,
+    )} to ${formatDate(lateDate)}`;
+    capacityIcon =
+      currentIntervention === INTERVENTIONS.LIMITED_ACTION ? (
+        <WarnLimitedAction />
+      ) : (
+        <WarnSocialDistancing />
+      );
   }
 
   const [calloutFillColor, calloutStrokeColor] = LAST_DATES_CALLOUT_COLORS[
     currentIntervention
   ];
 
+  // return (
+  //   <Callout
+  //     borderColor={calloutStrokeColor}
+  //     backgroundColor={calloutFillColor}
+  //   >
+  //     <div style={{ fontWeight: 'normal' }}>{actionText}</div>
+  //     {actionDateRange}
+  //   </Callout>
+  // );
+  const Title = ({ children }) => {
+    return (
+      <div
+        style={{
+          background: '#f2f2f2',
+          padding: '16px',
+          color: '#494949',
+          fontWeight: 'bold',
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+  const Content = ({ children }) => {
+    return (
+      <div
+        style={{
+          padding: '16px',
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+
   return (
-    <Callout
-      borderColor={calloutStrokeColor}
-      backgroundColor={calloutFillColor}
+    <div
+      style={{
+        marginTop: '2rem',
+        textAlign: 'left',
+        border: '1px solid #d5d5d5',
+        borderRadius: '4px',
+        borderSizing: 'border-box',
+        overflow: 'hidden',
+      }}
     >
-      <div style={{ fontWeight: 'normal' }}>{actionText}</div>
-      {actionDateRange}
-    </Callout>
+      <Title>Hospital capacity</Title>
+      <Content>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div>{capacityIcon}</div>
+          <div
+            style={{
+              paddingLeft: '16px',
+              color: '#4d4d4d',
+            }}
+          >
+            {shortActionText}
+          </div>
+        </div>
+      </Content>
+      <Title>Current Intervention style</Title>
+      <Content>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div
+            style={{
+              borderRadius: '9999px',
+              border: '2px solid #4d4d4d',
+            }}
+          >
+            <div
+              style={{
+                width: '20px',
+                height: '20px',
+                background: INTERVENTION_COLOR_MAP[currentIntervention],
+                borderRadius: '9999px',
+                border: '4px solid white',
+              }}
+            >
+              &nbsp;
+            </div>
+          </div>
+          <div
+            style={{
+              paddingLeft: '16px',
+              color: '#4d4d4d',
+            }}
+          >
+            {currentIntervention}
+          </div>
+        </div>
+      </Content>
+    </div>
   );
 };
 
